895aeae5771d37339edd026eea7f429e0061da3d
Ramazan Öğünç <ramazanogunc@hotmail.com>
2020-12-25 21:16:30 +0300

    Design several fragment review

--- app/src/main/java/com/ramo/xpandscrum/MainActivity.kt
+++ app/src/main/java/com/ramo/xpandscrum/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.ramo.xpandscrum
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import com.ramo.xpandscrum.R
 
 class MainActivity: AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
