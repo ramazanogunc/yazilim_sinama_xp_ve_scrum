db29ccab6df061df53712786dcf3a4074ddc0b32
Yusuf Sefa Aliş <yusufsefa61@gmail.com>
2021-01-09 17:45:24 +0300

    added date type converter

--- app/src/main/java/com/ramo/xpandscrum/Util.kt
+++ app/src/main/java/com/ramo/xpandscrum/Util.kt
@@ -1,8 +1,11 @@
 package com.ramo.xpandscrum
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
 import com.ramo.xpandscrum.model.User
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun Context.showToast(text: String) {
     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
@@ -17,4 +20,16 @@ fun predictDateFromDescription(description: String): Int {
 
 fun getFakeUsers(): List<User> {
     return listOf<User>(User("Ramo"), User("Yusuf"))
+}
+
+@SuppressLint("SimpleDateFormat")
+fun Date.getCurrentDate(): String {
+    val date = SimpleDateFormat("dd/M/yyyy hh:mm")
+    return date.format(this)
+}
+
+@SuppressLint("SimpleDateFormat")
+fun dateConvert(): Date {
+    val date = SimpleDateFormat("dd/M/yyyy hh:mm").parse(Date().getCurrentDate())
+    return date!!
 }
